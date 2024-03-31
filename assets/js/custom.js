feb96026bd3333248f649ed473b329d18833952f
RadhityaOben <radhityaoben@gmail.com>
2024-03-31 22:48:28 +0700

    Initialize Project

---
+++ assets/js/custom.js
@@ -0,0 +1,12 @@
+// Page Loader
+$(function () {
+  $("#topNavbar").load("layouts/top-navbar.html");
+});
+
+$(function () {
+  $("#header").load("layouts/header.html");
+});
+
+$(function () {
+  $("#footer").load("layouts/footer.html");
+});
