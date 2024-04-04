fa78c5476efb0e13fdf4c56153f0b03c779947e4
RadhityaOben <radhityaoben@gmail.com>
2024-04-04 12:05:03 +0700

    Finishing

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -10,3 +10,25 @@ $(function () {
 $(function () {
   $("#footer").load("layouts/footer.html");
 });
+
+// Berat Button
+var btnBerat1 = document.getElementById("btnBerat1");
+var btnBerat2 = document.getElementById("btnBerat2");
+
+$(btnBerat1).click(function () {
+  $(this).removeClass("btn-secondary");
+  $(this).addClass("btn-success");
+  $(this).addClass("btn-outline-secondary");
+  $(btnBerat2).removeClass("btn-success");
+  $(btnBerat2).removeClass("btn-outline-secondary");
+  $(btnBerat2).addClass("btn-secondary");
+});
+
+$(btnBerat2).click(function () {
+  $(this).removeClass("btn-secondary");
+  $(this).addClass("btn-success");
+  $(this).addClass("btn-outline-secondary");
+  $(btnBerat1).removeClass("btn-success");
+  $(btnBerat1).removeClass("btn-outline-secondary");
+  $(btnBerat1).addClass("btn-secondary");
+});
